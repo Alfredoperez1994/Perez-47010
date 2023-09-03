a716b732516c9738e3e548ebbab334d12855cd98
Alfredo Perez <pereztato@hotmail.com.ar>
2023-09-02 23:56:15 -0300

    tercera pre entrega

--- main.js
+++ main.js
@@ -1,72 +1,17 @@
-//saludo bienvenida
-let saludo = "hola";
-let nombre = prompt("ingrese su nombre");
-let saludoCompleto = saludo + nombre;
-alert(`${saludo} ${nombre}`);
-
-// cantidad a definir compra mayorista o minorista
-
-let cantidad = parseInt(prompt("cantidad a comprar"));
-
-if (cantidad >= 10) {
-    alert("su precio es mayorista")
-} else {
-    alert("su precio es minorista")
-}
-
-// tipo de alimento a comprar 
-
-let mascota = prompt("que mascota tiene?")
-
-switch (mascota) {
-    case "gato":
-        alert("alimento de gato");
-        break;
-
-    case "perro":
-        alert("no contamos con stock momentaneamente")
-        break;
-
-    default:
-        alert("no contamos con lo que estas buscando")
-        break;
-
-}
-
-class Alimento {
-    constructor(nombre, tamaño, precio){
+class Producto {
+    constructor(nombre, id, peso, precio, stock) {
         this.nombre = nombre;
-        this.tamaño = tamaño;
+        this.id = id;
+        this.peso = peso;
         this.precio = precio;
-        this.vendido = false;
+        this.stock = stock;
     }
 
-    vender(){
-        this.vender = true;
-    }
+};
 
-}
-
-const alimentosGato = [];
-
-alimentosGato.push(new Alimento("gato1","5kg", 450));
-alimentosGato.push(new Alimento("gato2","7.5kg", 800));
-alimentosGato.push(new Alimento("gato3","5kg", 300));
-alimentosGato.push(new Alimento("gato4","7.5kg", 500));
-
-
-
-let precio = parseInt(prompt("cuanto desea gastar?"))
-let filtrados = Alimento.filter((item) => item.precio === precio);
-
-filtrados.forEach((item) => {
-let mensaje = `
-Nombre: ${item.nombre}
-Tamaño: ${item.tamaño}
-Precio: $${item.precio}
-`;
-
-alert (mensaje);
-
-
-});
+const alimentos = [
+    { nombre: "Dog Chow",id: "1",peso: "20kg",precio: 450,stock: 100},
+    { nombre: "Pedigree", id: "2", peso: "15kg", precio: 210, stock: 100 },
+    { nombre: "Tiernitos", id: "3", peso: "15kg", precio: 370, stock: 100 },
+    { nombre: "Royal Canin mini adulto", id: "4", peso: "7,5kg", precio: 4530, stock: 100 }
+]
